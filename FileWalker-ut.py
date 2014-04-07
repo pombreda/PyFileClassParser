e6dbc29f95e7baa71fe641e505e9996d29281767
Sebastian Krueger <github@kruegermangelsen.com>
2014-04-07 23:57:39 +0200

    fixed invalid paths

--- FileWalker-ut.py
+++ FileWalker-ut.py
@@ -6,16 +6,16 @@ from FileWalker import FileWalker
 class TestSequence(unittest.TestCase):
 
 	def setUp(self):
-		self.folder = FileWalker('Broadcom SDK', '/Users/bastian/Jobb/Transmode/R21-EDU/Software/AIMVALLEY-Titan-R1_03_00-LD3.0-20121119')
+		self.folder = FileWalker('Broadcom SDK', './')
 
 	def tearDown(self):
 		del self.folder
 
 	def test_getFiles(self):
-		self.assertEqual(206, len(self.folder.getFiles()))
+		self.assertEqual(211, len(self.folder.getFiles()))
 
 	def test_checkPath(self):
-		self.assertEqual(self.folder.path, '/Users/bastian/Jobb/Transmode/R21-EDU/Software/AIMVALLEY-Titan-R1_03_00-LD3.0-20121119')
+		self.assertEqual(self.folder.path, './')
 		self.assertEqual(self.folder.name, 'Broadcom SDK')
 
 if __name__ == "__main__":
