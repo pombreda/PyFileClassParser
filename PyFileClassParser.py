e04987eb69745cc5cfd6e78bc5813e69f2c2668d
Sebastian Krueger <github@kruegermangelsen.com>
2014-04-07 22:44:42 +0200

    fixed main class in order to use revisied FileClass model

--- PyFileClassParser.py
+++ PyFileClassParser.py
@@ -30,12 +30,9 @@ class CodeAnalysis():
 
 	def __init__(self):
 		self.folder = []
-		self.isValid = False
-		self.userName = getpass.getuser()
 		self.db = {} # create a dictionary, file extensions to class names
 		self.sessions = [] # list of sessions, stores files and statistics for a number of classes per session
 		self.classes = [] # list of classes
-		self.classes.append(FileClass('other'))
 
 
 	def folderAdd(self, folderObject):
@@ -63,87 +60,11 @@ class CodeAnalysis():
 
 
 	def extAdd(self, type, extension):
-		if isinstance(extension, list):
-			for item in extension:
-				self.db[item] = type
-		elif isinstance(extension, basestring):
-			self.db[extension] = type
-		else:
-			return nonImplemented
-
-		# if not in self.classes
-		if len(self.classes) == 0:
-			self.classes.append(FileClass(type))
-		elif not self.__classExists(type):
-			self.classes.append(FileClass(type))
-		else:
-			return False
-		return True
-
-	def addFileToClassInSession(self, fileClass, pathToFile):
-			
-    	fileName, fileExtension = os.path.splitext(file)
-    	
-    	if fileExtension in self.db:
-    		fileClass = self.db[fileExtension]
-    		self.__addFileToClass(fileClass, os.path.join(dirpath, f))
-    	else:
-    		self.__addFileToClass('other', os.path.join(dirpath, f))
-		
-		self.isValid = True
-
-	def __classExists(self, type):
-		"""
-			check whether the class exists in the database
-		"""
-		for item in self.classes:
-			if type == item.name:
-				return True
-			continue
-
-		return False
-		
-
-	def __addFileToClass(self, type, fileToPath):
-		"""
-			TODO : move the code into a statistics module
-		"""
-		for item in self.classes:
-			
-			if type == item.name:
-
-				item.insertFile(fileToPath)
-				
-				item.statistics['numFiles'] += 1
-				fileSize = os.path.getsize(fileToPath)
-
-				item.statistics['sumSize'].add(fileSize)
-
-				# compute MAX
-				if item.statistics['maxSize'] < fileSize:
-					item.statistics['maxSize'].set(fileSize)
-				
-				# compute MIN
-				if item.statistics['minSize'] == 0:
-					item.statistics['minSize'].set(fileSize)
-				elif item.statistics['minSize'] > fileSize:
-					item.statistics['minSize'].set(fileSize)
-
-				# compute average
-				item.statistics['avgSize'].set(item.statistics['sumSize'].get() / item.statistics['numFiles']) 
-				return True
-			else:
-				continue
-
-		return False
-
-	def __getFileSize(self, path, fileName):
-		return os.path.getsize(os.path.join(path, fileName))
-
+		self.classes.append(FileClass(type, list(extension)))
 
 	def __str__(self):
 		str = ""
-		str += "File Analysis, created by %s\n" % self.userName
+		str += "File Analysis\n"
 		for item in self.classes:
 			str += " %s\n\n" % item
 		return str
